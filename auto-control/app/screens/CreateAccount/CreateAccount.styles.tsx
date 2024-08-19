c7d530c20d01094f897be1d30123540029d4aa8b
Rolf Matela <rolffreitas@yahoo.com.br>
2024-08-19 17:02:55 -0300

    :sparkles: feat: create the CreateAccount component and styles

---
+++ auto-control/app/screens/CreateAccount/CreateAccount.styles.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: "white",
+  },
+  container: {
+    display: "flex",
+    flex: 1,
+  },
+  upperContent: {
+    display: "flex",
+  },
+  middleContent: {
+    display: "flex",
+  },
+  bottomContent: {
+    display: "flex",
+  },
+});
+
+export default styles;
---
+++ auto-control/app/screens/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,32 @@
+// import from react and hooks
+import React from "react";
+
+// import from react native and expo
+import { View, SafeAreaView } from "react-native";
+
+// import from custom hooks
+
+// import from context
+
+// import from services
+
+// import from third party libraries
+
+// import from custom components
+import styles from "./CreateAccount.styles";
+
+// import from images and svgs
+
+// color scheme and variables
+
+export default function CreateAccount() {
+  return (
+    <SafeAreaView style={styles.mainContainer}>
+      <View style={styles.container}>
+        <View style={styles.upperContent}></View>
+        <View style={styles.middleContent}></View>
+        <View style={styles.bottomContent}></View>
+      </View>
+    </SafeAreaView>
+  )
+}
