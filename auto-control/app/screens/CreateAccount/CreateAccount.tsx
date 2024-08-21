88f429bb707981e754532b3db08ea6366233f4b5
Rolf Matela <rolffreitas@yahoo.com.br>
2024-08-21 10:16:49 -0300

    :recycle: refactor: did some minor changes

--- auto-control/app/screens/CreateAccount/CreateAccount.tsx
+++ auto-control/app/screens/CreateAccount/CreateAccount.tsx
@@ -2,7 +2,8 @@
 import React from "react";
 
 // import from react native and expo
-import { View, SafeAreaView } from "react-native";
+import { View, SafeAreaView, Text, TextInput, Pressable } from "react-native";
+import { router } from "expo-router";
 
 // import from custom hooks
 
@@ -20,13 +21,34 @@ import styles from "./CreateAccount.styles";
 // color scheme and variables
 
 export default function CreateAccount() {
+  const handleSubmit = () => {
+    // router.navigate("/login");
+  };
+
   return (
     <SafeAreaView style={styles.mainContainer}>
       <View style={styles.container}>
-        <View style={styles.upperContent}></View>
-        <View style={styles.middleContent}></View>
-        <View style={styles.bottomContent}></View>
+        <View style={styles.upperContent}>
+          <View style={styles.header}>
+            <Text>Crie sua conta Autocontrol</Text>
+          </View>
+        </View>
+        <View style={styles.middleContent}>
+          <View style={styles.inputContainer}>
+            <TextInput placeholder="Nome Completo" />
+            <TextInput placeholder="Email" />
+            <TextInput placeholder="Carteira Nacional de Habilitação" />
+            <TextInput placeholder="Senha" />
+          </View>
+        </View>
+        <View style={styles.bottomContent}>
+          <View style={styles.buttonContainer}>
+            <Pressable onPress={handleSubmit}>
+              <Text>Registrar</Text>
+            </Pressable>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
-  )
+  );
 }
