5dbbcc0e28410c8ff40b9341fffdeca14d42860d
Rolf Matela <rolffreitas@yahoo.com.br>
2024-08-21 16:47:38 -0300

    :recycle: refactor: did some minor changes

--- auto-control/constants/OnboardingSlides.ts
+++ auto-control/constants/OnboardingSlides.ts
@@ -1,23 +1,23 @@
-export default [
+export const Slides = [
   {
     id: 1,
     name: "slide1",
     title: "Fique tranquilo",
     description: "Não tenha surpresas com os gastos de seus veículos",
-    image: require("../../assets/images/relax-animation.json"),
+    image: require("../assets/images/relax-animation.json"),
   },
   {
     id: 2,
     name: "slide2",
     title: "Controle total",
     description: "Tenha controle total sobre os gastos de seus veículos",
-    image: require("../../assets/images/search-animation.json"),
+    image: require("../assets/images/search-animation.json"),
   },
   {
     id: 3,
     name: "slide3",
     title: "Relatórios",
     description: "Tenha relatórios detalhados sobre os gastos de seus veículos",
-    image: require("../../assets/images/schedule-animation.json"),
+    image: require("../assets/images/schedule-animation.json"),
   },
 ];
