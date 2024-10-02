d1c7204e7199bf52eea8f37904ae3d8b1d1cead3
Emanuel Vidal <emanuelvidalrm@gmail.com>
2024-10-02 02:33:28 -0300

    :sparkles: dashboard flatlist

--- auto-control/app/_layout.tsx
+++ auto-control/app/_layout.tsx
@@ -7,9 +7,10 @@ export default function Layout() {
   return (
     <SessionProvider>
       <LinearGradient colors={["#2282FF", "#326aee"]} style={{ flex: 1 }}>
-        <Stack>
+        <Stack initialRouteName="(app)">
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(app)" options={{ headerShown: false }} />
         </Stack>
       </LinearGradient>
     </SessionProvider>
