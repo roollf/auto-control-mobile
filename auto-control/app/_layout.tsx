53c30255b82b74ff47114b464bb0e3b23b92727a
Rolf Matela <rolffreitas@yahoo.com.br>
2024-10-02 02:03:32 -0300

    :sparkles: add new screen to app stack

--- auto-control/app/_layout.tsx
+++ auto-control/app/_layout.tsx
@@ -34,6 +34,7 @@ export default function Layout() {
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
           <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(app)" options={{ headerShown: false }} />
         </Stack>
       </SafeAreaView>
     </SessionProvider>
