9eb9f177e5155e7edb4618bb05657088f514db6d
Rolf Matela <rolffreitas@yahoo.com.br>
2024-10-16 11:02:58 -0300

    :construction: add vehicle and add expense screen

--- auto-control/app/dashboard/addexpense.tsx
+++ auto-control/app/dashboard/addexpense.tsx
@@ -1,9 +1,17 @@
+import { useEffect } from "react"
 import { Text, View } from "react-native"
 
 export default function AddExpense() {
+  useEffect(() => {
+    //getUserVehicles to fufill selector
+  })
+
   return (
     <View>
-      <Text>Add expense</Text>
+      <View>
+        <Text>Registre sua despesa</Text>
+        <Text>Veiculo</Text>
+      </View>
     </View>
   )
 }
