530966dcdce1c9c777ab42e456dd8a1dcd2a032c
Rolf Matela <rolffreitas@yahoo.com.br>
2024-12-02 15:01:16 -0300

    :sparkles: enhance Profile component to display user vehicles with mock data and implement vehicle fetching logic

--- auto-control/app/dashboard/profile.tsx
+++ auto-control/app/dashboard/profile.tsx
@@ -1,4 +1,4 @@
-import { Button, Text, TextInput, View } from "react-native"
+import { Button, Text, TextInput, View, ScrollView } from "react-native"
 
 import { useSession } from "@/contexts/ctx"
 
@@ -8,6 +8,9 @@ import React from "react"
 import { UserService } from "@/api/services"
 
 import { UserData } from "@/types/user/user.type"
+import { VehicleService } from "@/api/services/vehicleService"
+
+import AppInput from "@/components/AppInput/AppInput"
 
 const UserMock: { id: number; name: string; email: string; cnh: string } = {
   id: 1,
@@ -16,34 +19,86 @@ const UserMock: { id: number; name: string; email: string; cnh: string } = {
   cnh: "123456789",
 }
 
+const VehicleMock = [
+  {
+    id: 1,
+    brand: "Volkswagen",
+    model: "Gol",
+    year: 2019,
+    plate: "ABC-1234",
+  },
+  {
+    id: 2,
+    brand: "Chevrolet",
+    model: "Onix",
+    year: 2020,
+    plate: "DEF-5678",
+  },
+  {
+    id: 3,
+    brand: "Ford",
+    model: "Ka",
+    year: 2018,
+    plate: "GHI-9101",
+  },
+]
+
 export default function Profile() {
-  const { signOut } = useSession()
+  const { session, signOut } = useSession()
 
   const [name, setName] = React.useState("")
   const [email, setEmail] = React.useState("")
   const [cnh, setCnh] = React.useState("")
+  const [vehicles, setVehicles] = React.useState<
+    {
+      id: number
+      brand: string
+      model: string
+      year: number
+      plate: string
+    }[]
+  >([])
+
+  // chamada para a API
 
   const fetchUserData = async () => {
-    try {
-      const response: UserData = await UserService.getUser(UserMock.id)
-      setName(response.name)
-      setEmail(response.email)
-      setCnh(response.cnh)
-    } catch (error) {
-      console.error("Error fetching user data: ", error)
+    if (session?.user_id) {
+      const { user_id } = session
+      try {
+        const response: UserData = await UserService.getUser(user_id)
+        setName(response.name)
+        setEmail(response.email)
+        setCnh(response.cnh)
+      } catch (error) {
+        console.error("Error fetching user data: ", error)
+      }
+    }
+  }
+
+  const fetchVehicleData = async () => {
+    if (session?.token) {
+      const { token: userToken } = session
+      try {
+        const response = await VehicleService.getVehicles(userToken)
+        setVehicles(response)
+      } catch (error) {
+        console.error("Error fetching vehicle data: ", error)
+      }
     }
   }
 
   React.useEffect(() => {
     // fetchUserData()
+    // fetchVehicleData()
     setName(UserMock.name)
     setEmail(UserMock.email)
     setCnh(UserMock.cnh)
+    setVehicles(VehicleMock)
   }, [])
 
   return (
-    <View
-      style={{
+    <ScrollView
+      contentContainerStyle={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
@@ -52,70 +107,113 @@ export default function Profile() {
       }}
     >
       <FontAwesome5 name="user-circle" size={100} color="gray" />
-      <Text
+      <View
         style={{
-          fontSize: 24,
-          fontWeight: "bold",
-          alignSelf: "flex-start",
-          marginLeft: 40,
+          width: "80%",
+          alignItems: "center",
+          justifyContent: "center",
+          alignSelf: "center",
         }}
       >
-        Nome
-      </Text>
-      <TextInput
+        <AppInput
+          icon={
+            <FontAwesome5
+              name="user-circle"
+              size={20}
+              color="rgba(34, 130, 255, 0.7)"
+            />
+          }
+          placeholder="Nome"
+          value={name}
+          onChangeText={setName}
+        />
+      </View>
+      <View
         style={{
-          backgroundColor: "#eaeff4",
-          height: 40,
-          borderRadius: 8,
-          padding: 8,
           width: "80%",
-        }}
-        placeholder="Nome"
-        value={name}
-      />
-      <Text
-        style={{
-          fontSize: 24,
-          fontWeight: "bold",
-          alignSelf: "flex-start",
-          marginLeft: 40,
+          alignItems: "center",
+          justifyContent: "center",
+          alignSelf: "center",
         }}
       >
-        Email
-      </Text>
-      <TextInput
+        <AppInput
+          icon={
+            <FontAwesome5 name="at" size={20} color="rgba(34, 130, 255, 0.7)" />
+          }
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+      </View>
+      <View
         style={{
-          backgroundColor: "#eaeff4",
-          height: 40,
-          borderRadius: 8,
-          padding: 8,
           width: "80%",
-        }}
-        placeholder="Email"
-        value={email}
-      />
-      <Text
-        style={{
-          fontSize: 24,
-          fontWeight: "bold",
-          alignSelf: "flex-start",
-          marginLeft: 40,
+          alignItems: "center",
+          justifyContent: "center",
+          alignSelf: "center",
         }}
       >
-        CNH
-      </Text>
-      <TextInput
+        <AppInput
+          icon={
+            <FontAwesome5
+              name="id-badge"
+              size={20}
+              color="rgba(34, 130, 255, 0.7)"
+            />
+          }
+          placeholder="CNH"
+          value={cnh}
+          onChangeText={setCnh}
+        />
+      </View>
+      <View
         style={{
-          backgroundColor: "#eaeff4",
-          height: 40,
-          borderRadius: 8,
-          padding: 8,
           width: "80%",
+          alignItems: "flex-start",
+          justifyContent: "center",
         }}
-        placeholder="CNH"
-        value={cnh}
-      />
-      <Button title="Sign Out" onPress={signOut} />
-    </View>
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+          }}
+        >
+          Veículos
+        </Text>
+      </View>
+      <View
+        style={{
+          width: "80%",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          alignSelf: "center",
+          borderColor: "lightgrey",
+          borderWidth: 1,
+          padding: 10,
+        }}
+      >
+        {vehicles.map((vehicle, index) => (
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 10,
+              padding: 2,
+            }}
+          >
+            <FontAwesome5
+              name="car"
+              size={20}
+              color="rgba(34, 130, 255, 0.7)"
+            />
+            <Text>
+              {vehicle.model} {vehicle.brand} - {vehicle.plate}
+            </Text>
+          </View>
+        ))}
+      </View>
+      <Button title="Logout" onPress={signOut} />
+    </ScrollView>
   )
 }
